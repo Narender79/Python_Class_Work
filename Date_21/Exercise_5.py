4167f183ea12e1a252a95673166db6c924ab88a1
Narender <113188277+Narender79@users.noreply.github.com>
2022-12-22 19:35:19 +0530

    Update Exercise_5.py

--- Date_21/Exercise_5.py
+++ Date_21/Exercise_5.py
@@ -1,8 +1,13 @@
-# fl= [2, 3, 4, 5, 6, 7, 8]
-# sl = [4, 9, 16, 25, 36, 49, 64]
-# if(fl>sl):
-#     for i in range(0,len(fl)):
-
-
-# if(sl>fl):
-#     for i in range(0,len(sl)):
+fl= [2, 3, 4, 5, 6, 7, 8]
+sl = [4, 9, 16, 25, 36, 49, 64]
+res=[]
+for i in fl:
+    tup=()
+    dum=[]
+    for j in sl:
+        if i==j**(0.5):
+            dum.append(i)
+            dum.append(j)
+            tup=tuple(dum)
+    res.append(tup)
+print(res)    
